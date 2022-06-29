1b64bc9347d88a0c333a2c8805473d2ea2847506
Jade Cohen <Jade@Jades-MacBook-Pro.local>
2022-06-29 12:10:19 +0300

    APIS additions

--- routes/todo.js
+++ routes/todo.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const SQL = require("@nearform/sql");
 const { v4: uuid } = require("uuid");
-var format = require("date-format");
 const { query } = require("../lib/database");
 
 router.post("/", async (req, res) => {
   const { text } = req.body;
   const id = uuid();
-  const date = format();
+  const date = new Date().toDateString();
   const createdToDo = SQL`INSERT INTO todo(
         id,
         text,
@@ -23,6 +22,21 @@ router.get("/", async (req, res) => {
   const allToDos = await query(SQL`SELECT * FROM todo`);
   console.log(allToDos);
   res.send(allToDos);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const deleteItem = await query(SQL`DELETE FROM todo WHERE id=${id}`);
+  console.log(deleteItem);
+  res.send(deleteItem);
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const updateCompleted = await query(
+    SQL`UPDATE todo SET completed=true WHERE id = ${id}`
+  );
+  res.send(updateCompleted);
 });
 
 module.exports = router;
